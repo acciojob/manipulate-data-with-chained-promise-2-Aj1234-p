270f34b248b0dfdf0d735af43c9f2d4f176d5cf0
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-08-24 07:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function filterEvenNumber(arr){
   return new Promise((resolve,reject)=>{
       setTimeout(()=>{
         resolve(evenArray);
-      },1000)
+      },4000)
   })
 }
 
@@ -33,7 +33,7 @@ function multiplyEvenNumbers(arr){
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{      
        resolve(multiplyArray);
-    },3000);
+    },7000);
   })
 }
 let evenData;
